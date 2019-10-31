197d2e1be891e4a4b74c1b0c37d3de3c1a90bbee
Whittney Davis <wcdavis89@yahoo.com>
2019-10-31 17:40:45 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,41 @@
+def using_push(array,element)
+ array.push(element)  
+end
+
+def using_unshift(array,element)
+  array.unshift(element)
+end
+
+
+def using_pop(array)
+  deleted_string=array.pop
+end
+
+def pop_with_args(array)
+  small_dogs=array.pop(2)
+end
+
+def using_shift(array)
+  im_so_over_this_city=array.shift
+end
+
+def shift_with_args(array)
+  brands_removed=array.shift(2)
+end
+
+def using_concat(array1,array2)
+   all_my_favs=array1.concat(array2)
+end
+
+def using_insert(array,element)
+   new_array=array.insert(index,element)
+end
+
+def using_uniq(array)
+   new_array=array.uniq(array)
+end
+
+def using_flatten(array1,array2)
+   flat_array=array1(array2)
+end
+
